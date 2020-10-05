cea01f002c5fc6c3b1f84737e8971eea4ddf0cd8
Matt Kubej <mkubej@gmail.com>
2020-10-04 22:37:44 -0400

    build: wire up backend

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,5 +1,23 @@
-import { createServer } from 'http';
+import Koa from 'koa';
+import { ApolloServer, gql } from 'apollo-server-koa';
 
-createServer().listen(4000, () => {
-  console.log('listening on port 4000');
-});
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const app = new Koa();
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`),
+);
