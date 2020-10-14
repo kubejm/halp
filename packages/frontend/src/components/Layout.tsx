6ccc53920eaa3dd23671d9e70a29130cf83c7beb
Matt Kubej <mkubej@gmail.com>
2020-10-13 22:50:24 -0400

    build(frontend): introduce prettier and eslint

--- packages/frontend/src/components/Layout.tsx
+++ packages/frontend/src/components/Layout.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+interface Props {
+  children?: React.ReactNode
+}
+
+export default function Layout({ children }: Props) {
   return (
-    <div className="bg-gray- 100 min-h-screen flex flex-col">
-      {children}
-    </div>
+    <div className="bg-gray- 100 min-h-screen flex flex-col">{children}</div>
   );
 }
