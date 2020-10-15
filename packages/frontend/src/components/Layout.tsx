3c58f383faafcb109027aae1b79e671a02334abb
Matt Kubej <mkubej@gmail.com>
2020-10-14 21:57:45 -0400

    feat(frontend): add header

--- packages/frontend/src/components/Layout.tsx
+++ packages/frontend/src/components/Layout.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
 
 interface Props {
-  children?: React.ReactNode
+  children?: React.ReactNode;
+}
+
+function Logo() {
+  return (
+    <div className="text-white">Halp!</div>
+  );
+}
+
+function Header() {
+  return (
+    <nav className="flex items-center justify-between bg-purple-800 px-8 h-16">
+      <Logo />
+    </nav>
+  );
 }
 
 export default function Layout({ children }: Props) {
   return (
-    <div className="bg-gray- 100 min-h-screen flex flex-col">{children}</div>
+    <div className="bg-gray-100 min-h-screen flex flex-col">
+      <Header />
+      {children}
+    </div>
   );
 }
