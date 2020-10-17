e31a947b3115fbd9d6270dbf31872ee2327230b6
Matt Kubej <mkubej@gmail.com>
2020-10-16 23:18:49 -0400

    refactor(frontend): props for QuoteSummary

--- packages/frontend/src/components/question-list/index.tsx
+++ packages/frontend/src/components/question-list/index.tsx
@@ -1,43 +1,77 @@
 import React from 'react';
 
-function QuestionSummary() {
+interface Tag {
+  label: string;
+}
+
+interface QuestionSummaryProps {
+  answers?: number;
+  askedTime: string;
+  author: string;
+  excerpt: string;
+  question: string;
+  tags: Tag[];
+  views?: number;
+  votes?: number;
+}
+
+function QuestionSummary(props: QuestionSummaryProps) {
   return (
     <div className="flex border-l border-b p-4 divide-x">
       <div className="pr-3 text-center">
         <div className="pt-2">
-          <div>0</div>
+          <div>{props.votes}</div>
           <div className="text-xs text-gray-700">votes</div>
         </div>
         <div className="pt-4 text-gray-700">
-          <div>0</div>
+          <div>{props.answers}</div>
           <div className="text-xs">answers</div>
         </div>
         <div className="pt-4 text-xs text-gray-600">
-          <div>0</div>
+          <div>{props.views}</div>
           <div>views</div>
         </div>
       </div>
-      <div className="flex justify-between flex-wrap pl-4">
-        <div className="min-w-full text-lg pb-2">How do I undo the most recent local commits in Git?</div>
-        <div className="min-w-full text-sm text-gray-700 pb-2">I accidentally committed the wrong files to Git, but I haven't pushed the commit to the server yet. How can I undo those commits from the local repository?</div>
+      <div className="flex flex-grow justify-between flex-wrap pl-4">
+        <div className="min-w-full text-lg">{props.question}</div>
+        <div className="min-w-full text-sm text-gray-700 pb-2">
+          {props.excerpt}
+        </div>
         <div className="flex flex-row items-start text-xs pb-2">
-          <div className="text-gray-700 bg-gray-400 rounded text-center px-2 py-1 mr-1">git</div>
-          <div className="text-gray-700 bg-gray-400 rounded text-center px-2 py-1 mr-1">git</div>
-          <div className="text-gray-700 bg-gray-400 rounded text-center px-2 py-1 mr-1">git</div>
+          {props.tags.map((tag: Tag) => (
+            <div className="text-gray-700 bg-gray-400 rounded text-center px-2 py-1 mr-1">
+              {tag.label}
+            </div>
+          ))}
         </div>
         <div className="flex flex-col">
-          <div className="text-xs text-gray-500">asked 1 min ago</div>
-          <div className="text-gray-700 text-sm align-middle">matt</div>
+          <div className="text-xs text-gray-500">{props.askedTime}</div>
+          <div className="text-gray-700 text-sm align-middle">
+            {props.author}
+          </div>
         </div>
       </div>
     </div>
   );
+}
+
+QuestionSummary.defaultProps = {
+  answers: 0,
+  views: 0,
+  votes: 0,
+  tags: [],
 };
 
 export default function QuestionList() {
   return (
     <>
-      <QuestionSummary />
+      <QuestionSummary
+        askedTime={'asked 1 min ago'}
+        author={'matt'}
+        excerpt={'I do not know how to use. How do I do it?'}
+        question={'How do I git?'}
+        tags={[{ label: 'git' }]}
+      />
     </>
   );
 }
