466342ce217182598a7fa9c192815b3ef7e23126
Matt Kubej <mkubej@gmail.com>
2020-10-22 23:52:42 -0400

    refactor(backend): unnecessary tag array

--- packages/backend/src/validators/question.validator.ts
+++ packages/backend/src/validators/question.validator.ts
@@ -1,6 +1,5 @@
 import { Field, InputType } from 'type-graphql';
-import { IsArray, IsOptional, IsString, ValidateNested } from 'class-validator';
-import { TagValidator } from './tag.validator';
+import { IsString } from 'class-validator';
 
 @InputType()
 export class QuestionValidator {
@@ -15,10 +14,4 @@ export class QuestionValidator {
   @Field()
   @IsString()
   question!: string;
-
-  @Field(() => [TagValidator])
-  @IsArray()
-  @IsOptional()
-  @ValidateNested()
-  tags?: TagValidator[];
 }
