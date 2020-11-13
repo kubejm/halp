327e008285fc1e90fc2ef10fc3a7e15d4f0ba11a
Matt Kubej <mkubej@gmail.com>
2020-11-12 22:56:59 -0500

    feat(frontend): hover on sign in link

--- packages/frontend/src/components/layout/menu-bar/MenuLink.tsx
+++ packages/frontend/src/components/layout/menu-bar/MenuLink.tsx
@@ -8,7 +8,10 @@ interface MenuLinkProps {
 
 export default function MenuLink({ to, label }: MenuLinkProps) {
   return (
-    <Link to={to} className="text-white ml-4">
+    <Link
+      to={to}
+      className="text-white ml-4 hover:bg-purple-600 rounded px-3 py-2"
+    >
       {label}
     </Link>
   );
