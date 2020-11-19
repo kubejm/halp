64845cc7a236c6451306d96a04e7084618139c44
Matt Kubej <mkubej@gmail.com>
2020-11-18 22:16:02 -0500

    build(backend): env vars for postgres

--- packages/backend/src/orm.config.ts
+++ packages/backend/src/orm.config.ts
@@ -1,13 +1,11 @@
 import { MikroORM } from '@mikro-orm/core';
 
-// TODO: inject with environment variables
 export default {
   entities: ['./src/entities'],
   dbName: 'postgres',
   type: 'postgresql',
-  host: 'postgres',
-  port: 5432,
-  user: 'postgres',
-  password: 'halp',
-  debug: true,
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
 } as Parameters<typeof MikroORM.init>[0];
