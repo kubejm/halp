ed41127a6768729737a9410c44e61fafdff23b61
Matt Kubej <mkubej@gmail.com>
2020-11-22 22:58:25 -0500

    feat(frontend): cleaner looking buttons

--- packages/frontend/src/components/layout/menu-bar/unauthenticated-menu/index.tsx
+++ packages/frontend/src/components/layout/menu-bar/unauthenticated-menu/index.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+// TODO: abstract buttons out into shared styles
 export default function UnauthenticatedMenu() {
   return (
-    <div>
-      <Link to="/sign-in">
-        <button className="text-purple-700 bg-gray-50 text-sm ml-3 border border-purple-400 rounded py-1 px-2 hover:bg-purple-900 hover:text-white transition duration-300 ease-in-out focus:outline-none">
-          Sign In
-        </button>
+    <div className="flex flex-row items-center text-sm">
+      <Link
+        to="/sign-in"
+        className="ml-4 py-1 px-2 rounded bg-white text-purple-500 hover:bg-purple-500 hover:text-white border-2 border-white hover:border-purple-200 transition duration-300 ease-in-out"
+      >
+        Sign In
       </Link>
-      <Link to="/sign-up">
-        <button className="text-white bg-purple-900 text-sm ml-3 border border-purple-400 rounded py-1 px-2 hover:bg-gray-50 hover:text-purple-700 transition duration-300 ease-in-out focus:outline-none">
-          Sign Up
-        </button>
+      <Link
+        to="/sign-up"
+        className="ml-4 py-1 px-2 rounded bg-purple-500 text-white hover:bg-white hover:text-purple-600 border-2 border-purple-500 hover:border-purle-100 transition duration-300 ease-in-out"
+      >
+        Sign Up
       </Link>
     </div>
   );
