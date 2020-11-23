36a4fc139a31b20e1493bbd434b3209f66b811dd
Matt Kubej <mkubej@gmail.com>
2020-11-22 22:38:49 -0500

    feat(frontend): first attempt at button styling

--- packages/frontend/src/components/layout/menu-bar/unauthenticated-menu/index.tsx
+++ packages/frontend/src/components/layout/menu-bar/unauthenticated-menu/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import MenuLink from '../MenuLink';
+import { Link } from 'react-router-dom';
 
 export default function UnauthenticatedMenu() {
   return (
     <div>
-      <MenuLink to="/sign-in" label="Sign In" />
-      <MenuLink to="/sign-up" label="Sign Up" />
+      <Link to="/sign-in">
+        <button className="text-purple-700 bg-gray-50 text-sm ml-3 border border-purple-400 rounded py-1 px-2 hover:bg-purple-900 hover:text-white transition duration-300 ease-in-out focus:outline-none">
+          Sign In
+        </button>
+      </Link>
+      <Link to="/sign-up">
+        <button className="text-white bg-purple-900 text-sm ml-3 border border-purple-400 rounded py-1 px-2 hover:bg-gray-50 hover:text-purple-700 transition duration-300 ease-in-out focus:outline-none">
+          Sign Up
+        </button>
+      </Link>
     </div>
   );
 }
