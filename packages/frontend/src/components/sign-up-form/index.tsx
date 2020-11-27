deae2683c7430c790fe5f2ea6b278bd5e561a990
Matt Kubej <mkubej@gmail.com>
2020-11-27 12:51:33 -0500

    refactor(frontend): abstract out input for sign in

--- packages/frontend/src/components/sign-up-form/index.tsx
+++ packages/frontend/src/components/sign-up-form/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
+import { FormProvider, useForm } from 'react-hook-form';
 import { graphql, useMutation } from 'react-relay/hooks';
 import { signUpFormMutation } from '../../__generated__/signUpFormMutation.graphql';
 import { useHistory } from 'react-router-dom';
 import { useStore } from '../../store';
 import { ValidationError } from '../../utils';
+import { Input } from '../form';
 
 export default function SignInForm() {
-  const { register, handleSubmit, errors, setError } = useForm();
+  const formMethods = useForm();
   const [commit, isInFlight] = useMutation<signUpFormMutation>(graphql`
     mutation signUpFormMutation($input: SignUpInput!) {
       signUp(input: $input) {
@@ -30,7 +31,7 @@ export default function SignInForm() {
       },
       onError(error) {
         if (error instanceof ValidationError) {
-          setError(error.property, {
+          formMethods.setError(error.property, {
             type: 'server',
             message: error.constraint,
           });
@@ -47,52 +48,22 @@ export default function SignInForm() {
     <>
       <div className="flex">
         <div className="w-auto inline-block m-auto mt-6">
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="bg-white shadow-md rounded p-6"
-          >
-            <div className="mb-6">
-              <label className="block text-gray-700 text-sm font-bold mb-2">
-                Username
-                <input
-                  name="username"
-                  className="border p-2 mt-3 w-full"
-                  ref={register({ required: true })}
-                />
-              </label>
-            </div>
-            <div className="mb-6">
-              <label className="block text-gray-700 text-sm font-bold mb-2">
-                Email
-                <input
-                  name="email"
-                  className="border p-2 mt-3 w-full"
-                  ref={register({ required: true })}
-                />
-              </label>
-              {errors.email && (
-                <span className="text-sm text-red-600">
-                  {errors.email.message}
-                </span>
-              )}
-            </div>
-            <div className="mb-6">
-              <label className="block text-gray-700 text-sm font-bold mb-2">
-                Password
-                <input
-                  name="password"
-                  className="border p-2 mt-3 w-full"
-                  ref={register({ required: true })}
-                />
-              </label>
-            </div>
-            <input
-              type="submit"
-              value="Sign Up"
-              className="w-full bg-purple-600 hover:bg-purple-500 text-white font-semibold p-3 cursor-pointer"
-              disabled={isInFlight}
-            />
-          </form>
+          <FormProvider {...formMethods}>
+            <form
+              onSubmit={formMethods.handleSubmit(onSubmit)}
+              className="bg-white shadow-md rounded p-6"
+            >
+              <Input label="Username" name="username" />
+              <Input label="Email" name="email" />
+              <Input label="Password" name="password" />
+              <input
+                type="submit"
+                value="Sign Up"
+                className="w-full bg-purple-600 hover:bg-purple-500 text-white font-semibold p-3 cursor-pointer"
+                disabled={isInFlight}
+              />
+            </form>
+          </FormProvider>
         </div>
       </div>
     </>
