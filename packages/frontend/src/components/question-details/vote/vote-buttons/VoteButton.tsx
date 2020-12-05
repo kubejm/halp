b853511836fc4627809f4608f2e341432a07c980
Matt Kubej <mkubej@gmail.com>
2020-12-05 13:09:32 -0500

    feat(frontend): disabled vote handling and styling

--- packages/frontend/src/components/question-details/vote/vote-buttons/VoteButton.tsx
+++ packages/frontend/src/components/question-details/vote/vote-buttons/VoteButton.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import clsx from 'clsx';
+import { useStore } from '../../../../store';
 
 interface Props {
   children?: React.ReactNode;
@@ -8,11 +9,17 @@ interface Props {
 }
 
 export default function VoteButton({ children, onClick, hasUserVoted }: Props) {
+  const isAuthenticated = useStore((state) => state.isAuthenticated);
+
   return (
     <button
       onClick={onClick}
-      disabled={hasUserVoted}
-      className={clsx('focus:outline-none', hasUserVoted && 'text-purple-600')}
+      disabled={!isAuthenticated || hasUserVoted}
+      className={clsx(
+        'focus:outline-none',
+        hasUserVoted && 'text-purple-600',
+        !isAuthenticated && 'disabled:opacity-60'
+      )}
     >
       {children}
     </button>
