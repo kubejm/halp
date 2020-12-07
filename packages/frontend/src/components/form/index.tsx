d77f983feb99b5452cef7b635d52b6de8bf2dcfa
Matt Kubej <mkubej@gmail.com>
2020-12-06 22:20:47 -0500

    docs(frontend): reminder for abstraction

--- packages/frontend/src/components/form/index.tsx
+++ packages/frontend/src/components/form/index.tsx
@@ -1,3 +1,4 @@
+// TODO: abstract out commonalities in these inputs
 export { default as Input } from './Input';
 export { default as TagInput } from './TagInput';
 export { default as TextArea } from './TextArea';
