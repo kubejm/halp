b90ba2af494c3fa5c29711273b711891531ae525
Matt Kubej <mkubej@gmail.com>
2020-12-13 23:28:49 -0500

    feat(frontend): placeholder components, sorting

--- packages/frontend/src/screens/Home.tsx
+++ packages/frontend/src/screens/Home.tsx
@@ -5,6 +5,23 @@ import { QuestionList } from '../components';
 export default function Home() {
   return (
     <>
+      <div className="flex flex-wrap bg-white border-l border-b p-4">
+        <h1 className="text-xl min-w-full">Questions</h1>
+        <div className="flex justify-between min-w-full">
+          <div className="flex flex-col-reverse">
+            <div className="text-xs text-gray-500">1234 questions asked</div>
+          </div>
+          <div>
+            <ul className="flex list-none rounded text-xs text-purple-500">
+              <li className="py-2 px-3 border border-r-0 bg-purple-500 text-white border rounded-l">
+                New
+              </li>
+              <li className="py-2 px-3 border border-r-0">Active</li>
+              <li className="py-2 px-3 border rounded-r">Most Votes</li>
+            </ul>
+          </div>
+        </div>
+      </div>
       <QuestionList />
     </>
   );
