20ec831cf900d6f3a70824aeebe33d5aa1b6d86a
Matt Kubej <mkubej@gmail.com>
2020-12-18 09:47:15 -0500

    refactor(frontend): formatting

--- packages/frontend/src/components/question-sorter/index.tsx
+++ packages/frontend/src/components/question-sorter/index.tsx
@@ -17,7 +17,9 @@ export default function QuestionSorter() {
       <h1 className="text-xl min-w-full">Questions</h1>
       <div className="flex justify-between min-w-full">
         <div className="flex flex-col-reverse">
-          <div className="text-xs text-gray-500">{questionCount} questions asked</div>
+          <div className="text-xs text-gray-500">
+            {questionCount} questions asked
+          </div>
         </div>
         <div>
           <ul className="flex list-none rounded text-xs text-purple-500">
