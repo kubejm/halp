1751a3c3918f8eab993d95cd08c519e0c1840d7b
Matt Kubej <mkubej@gmail.com>
2020-12-18 09:51:32 -0500

    feat(frontend): link from question sorter

--- packages/frontend/src/components/question-sorter/index.tsx
+++ packages/frontend/src/components/question-sorter/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { graphql, useLazyLoadQuery } from 'react-relay/hooks';
 import { questionSorterQuery } from '../../__generated__/questionSorterQuery.graphql';
+import { Link } from 'react-router-dom';
 
+// TODO: clean up Link elements
+// TODO: add hover for buttons
+// TODO: change selected
 export default function QuestionSorter() {
   const { questionCount } = useLazyLoadQuery<questionSorterQuery>(
     graphql`
@@ -23,11 +27,17 @@ export default function QuestionSorter() {
         </div>
         <div>
           <ul className="flex list-none rounded text-xs text-purple-500">
-            <li className="py-2 px-3 border border-r-0 bg-purple-500 text-white border rounded-l">
-              New
-            </li>
-            <li className="py-2 px-3 border border-r-0">Active</li>
-            <li className="py-2 px-3 border rounded-r">Most Votes</li>
+            <Link to={'/'}>
+              <li className="py-2 px-3 border border-r-0 bg-purple-500 text-white border rounded-l">
+                New
+              </li>
+            </Link>
+            <Link to={'/?orderBy=ACTIVE'}>
+              <li className="py-2 px-3 border border-r-0">Active</li>
+            </Link>
+            <Link to={'/?orderBy=VOTES'}>
+              <li className="py-2 px-3 border rounded-r">Most Votes</li>
+            </Link>
           </ul>
         </div>
       </div>
