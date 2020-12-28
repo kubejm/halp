a370d1a6e58dfd3dc4be45f219f5ed50422ffeda
Matt Kubej <mkubej@gmail.com>
2020-12-28 00:58:01 -0500

    feat(backend): sort tags alphabetically

--- packages/backend/src/services/tag.service.ts
+++ packages/backend/src/services/tag.service.ts
@@ -1,8 +1,13 @@
+import { QueryOrder } from '@mikro-orm/core';
 import { Tag } from '../entities';
 import { Context } from '../types';
 
 export function getTags({ ctx }: Context) {
-  return ctx.em.getRepository(Tag).findAll();
+  return ctx.em.getRepository(Tag).findAll({
+    orderBy: {
+      name: QueryOrder.ASC,
+    },
+  });
 }
 
 export async function getTagQuestionCount(tag: Tag) {
