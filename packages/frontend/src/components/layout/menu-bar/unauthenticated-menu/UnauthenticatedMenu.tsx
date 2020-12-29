f8d675be3ba88b3e96de4e29a9361997f0bbe09b
Matt Kubej <mkubej@gmail.com>
2020-12-29 15:08:23 -0500

    refactor(frontend): consolidate css classes

--- packages/frontend/src/components/layout/menu-bar/unauthenticated-menu/UnauthenticatedMenu.tsx
+++ packages/frontend/src/components/layout/menu-bar/unauthenticated-menu/UnauthenticatedMenu.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import clsx from 'clsx';
 
 export default function UnauthenticatedMenu() {
+  const defaultStyles =
+    'ml-4 py-1 px-2 rounded border-2 transition duration-300 ease-in-out';
+
   return (
     <nav className="flex flex-row items-center text-sm">
       <Link
         to="/sign-in"
-        className="ml-4 py-1 px-2 rounded bg-white text-purple-500 hover:bg-purple-500 hover:text-white border-2 border-white hover:border-purple-200 transition duration-300 ease-in-out"
+        className={clsx(
+          defaultStyles,
+          'bg-white text-purple-500 hover:bg-purple-500 hover:text-white border-white hover:border-purple-200'
+        )}
       >
         Sign In
       </Link>
       <Link
         to="/sign-up"
-        className="ml-4 py-1 px-2 rounded bg-purple-500 text-white hover:bg-white hover:text-purple-600 border-2 border-purple-500 hover:border-purle-100 transition duration-300 ease-in-out"
+        className={clsx(
+          defaultStyles,
+          'bg-purple-500 text-white hover:bg-white hover:text-purple-600 border-purple-500 hover:border-purle-100'
+        )}
       >
         Sign Up
       </Link>
